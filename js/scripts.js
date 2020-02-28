27c777a98bf2127acdbd7aadb052973acc620c35
Ben Wilson and Tim Klaus <tim@>
2020-02-27 20:25:12 -0800

    initial setup

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $("#groceries").submit(function(event) {
+    event.preventDefault();
+    // var items = ["input#item1", "input#item2", "input#item3", "input#item4", "input#item5", "input#item6"]; 
+    var items = ["item1", "item2", "item3", "item4", "item5", "item6"];
+    console.log(items)
+    var stuff = []
+    items.forEach(function(item) {
+       stuff.push($("input#" + item).val());
+    });
+    console.log(stuff.toUpperCase().sort());
+
+    debugger
+    // var stuffs = items.map(function(item) {
+     // toString($("input#" + item).val());
+    //});
+      //var finalList = [];
+    //var finalList = modList.sort();
+    //console.log(finalList);
+    //$("#list").text("<li>" +  "</li>")
+
+    // console.log(modList);
+    // debugger
+    });
+});
